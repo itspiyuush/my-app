1f1a2b111159e72464632d014938b7331d4a2906
Barapatre piyush <pbarapatre60@gmail.com>
2023-04-14 15:45:52 +0530

    14 march second commit

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,25 +1,14 @@
 import "./posts.css";
 import Past from "../past/Past";
-import {  React, Link } from "react-router-dom";
+import {  React } from "react-router-dom";
 
 export default function Posts() {
   return (
     <div className="posts">
-       <Link className="linkc" to="/post/:id">
-               <Past/>
-      </Link>
-      <Link className="linkc" to="/post/:id">
-               <Past/>
-      </Link>
-      <Link className="linkc" to="/post/:id">
-               <Past/>
-      </Link>
-      <Link className="linkc" to="/post/:id">
-               <Past/>
-      </Link>
-      <Link className="linkc" to="/post/:id">
-               <Past/>
-      </Link>
+        <Past/>
+        <Past/>
+        <Past/>
+        <Past/>
     </div>
   );
 }
